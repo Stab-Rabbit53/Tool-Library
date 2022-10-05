c133c7523d352f03ecac9d7573c48ac6d5f7f248
Akash <akash.vinay.patel@gmail.com>
2022-10-04 20:43:22 -0700

    saved changes for db connection

---
+++ BackEnd/Controllers/itemController.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const db = require('../models/models.js');
+//require('dotenv').config();
+//const fetch = require('node-fetch');
+//import database here
+
+const itemController = {};
+
+itemController.addTool = (req, res, next) => {
+    //interact with DB here
+}
+
+itemController.deleteTool = (req, res, next) => {
+    //interact with DB here
+}
+
+itemController.getUserItemsList = (req, res, next) => {
+    //interact with DB here
+}
+
+itemController.getGlobalItemsList = (req, res, next) => {
+    //interact with DB here
+}
+
+
+module.exports = itemController;
