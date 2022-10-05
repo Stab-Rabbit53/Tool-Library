a8fb3dfeebab2c7197877ef3580d6f221c833e03
Akash <akash.vinay.patel@gmail.com>
2022-10-04 17:29:30 -0700

    created backend skeleton

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
-const path = require('node:path');
+//const path = require('path');
 
-//importing controllers 
-const userController = require('./Controllers/user-controller')      
-const userRoute = require('./Routes/user-route')
-const itemRoute = require('./Routes/item-router')
+//importing routers
+const mainPageRouter = require('./Routes/mainPageRouter');
+const userRouter = require('./Routes/userRouter');
 
 app.use(express.json());
 
-// handle sign up and login functionalities 
-app.use('/api/user/items', itemRoute);
-app.use('/api/user', userRoute);
+//any call to '/api' will get routed to this server
+//define routes
+app.use('/api/mainPage', mainPageRouter);
+app.use('/api/user', userRouter);
+
 
 //global error handler
 app.use('/', 
-  (err, request, response) => {
-    response.status(400).send('Server had a misc middleware error: ' + err)
+  (err, req, res) => {
+    req.status(400).send('Server had a misc middleware error: ' + err)
 });
 
 app.listen(3000, () => {
