e2eebf0d04b7ba8c2a87545691a6e1d9360e3742
JP Keith <jpkeith15@gmail.com>
2022-10-06 15:25:30 -0700

    Co-authored-by: Tim Kang <tkang611@users.noreply.github.com> Co-authored-by: akashpatel1198 <akashpatel1198@users.noreply.github.com> Co-authored-by: Lisa Tian <lisatiann@users.noreply.github.com> Co-authored-by: CELINE L. <ccelineleung@users.noreply.github.com>

--- FrontEnd/newComponents/Homepage.js
+++ FrontEnd/newComponents/Homepage.js
@@ -3,15 +3,19 @@ import UserItemsContainer from './UserItems';
 import BrowseContainer from './Browse';
 import AddTools from './AddNewTool';
 import BorrowedItems from './BorrowedItems';
+import { ItemContextProvider } from '../contexts/itemContext';
+
 
 function Homepage({ username }) {
   return (
     <div>
-      <h1>HomePage</h1>
-      <AddTools username={username} />
-      <UserItemsContainer username={username} />
-      <BorrowedItems username={username} />
-      <BrowseContainer username={username} />
+      <ItemContextProvider>
+        <h1>Welcome, {username.toUpperCase()}</h1>
+        <AddTools username={username} />
+        <UserItemsContainer username={username} />
+        <BorrowedItems username={username} />
+        <BrowseContainer username={username} />
+      </ItemContextProvider>
     </div>
   );
 }
